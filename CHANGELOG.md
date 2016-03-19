c11e1ffdb34facd4ab0436838461ee436b7ece15
Artyom <yom@artyom.me>
2016-03-19 17:58:58 +0300

    Fix everything and release another version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-# 0.1.0.0
+# 0.2.0.0
+
+* Change types (the previous version was unusable).
+* Actually return the value returned by the Lucid action.
+
+# 0.1.0.0 (deprecated)
 
 First release.
--- lib/Web/Spock/Lucid.hs
+++ lib/Web/Spock/Lucid.hs
@@ -1,24 +1,39 @@
 {-# LANGUAGE
-OverloadedStrings
+OverloadedStrings,
+RankNTypes
   #-}
 
 
 module Web.Spock.Lucid
 (
   lucid,
+  lucidIO,
 )
 where
 
 
-import Control.Monad.Trans.Class
+import Data.Functor.Identity
 import Control.Monad.IO.Class
 import Web.Spock
 import Lucid.Base
+import Blaze.ByteString.Builder
 
 
 -- | Render HTML and send as response body. Content-type will be @text/html@.
-lucid :: MonadIO m => HtmlT m a -> ActionCtxT ctx m a
+lucid :: MonadIO m => Html a -> ActionCtxT cxt m a
 lucid x = do
   setHeader "Content-Type" "text/html; charset=utf-8"
-  lazyBytes =<< lift (renderBST x)
+  let Identity (render, a) = runHtmlT x
+  lazyBytes (toLazyByteString (render mempty))
+  return a
 {-# INLINE lucid #-}
+
+-- | Like 'lucid', but for @HtmlT IO@.
+lucidIO :: MonadIO m => HtmlT IO a -> ActionCtxT cxt m a
+lucidIO x = do
+  setHeader "Content-Type" "text/html; charset=utf-8"
+  (render, a) <- liftIO (runHtmlT x)
+  lazyBytes (toLazyByteString (render mempty))
+  return a
+{-# INLINE lucidIO #-}
+
