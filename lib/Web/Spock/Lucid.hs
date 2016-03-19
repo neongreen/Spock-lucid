88b7d2f6837e0490f53dbac4480ba7df44af6611
Artyom <yom@artyom.me>
2016-03-19 15:52:11 +0300

    Write code

--- lib/Web/Spock/Lucid.hs
+++ lib/Web/Spock/Lucid.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE
+OverloadedStrings
+  #-}
+
+
 module Web.Spock.Lucid
 (
+  lucid,
 )
 where
+
+
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
+import Web.Spock
+import Lucid.Base
+
+
+-- | Render HTML and send as response body. Content-type will be @text/html@.
+lucid :: MonadIO m => HtmlT m a -> ActionCtxT ctx m a
+lucid x = do
+  setHeader "Content-Type" "text/html; charset=utf-8"
+  lazyBytes =<< lift (renderBST x)
+{-# INLINE lucid #-}
