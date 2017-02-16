77978368852eb0976a155d0539b3541314cd87f8
Artyom <yom@artyom.me>
2017-02-16 13:50:34 +0300

    Simplify the implementation

--- lib/Web/Spock/Lucid.hs
+++ lib/Web/Spock/Lucid.hs
@@ -1,8 +1,5 @@
-{-# LANGUAGE
-CPP,
-OverloadedStrings,
-RankNTypes
-  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 
 
 module Web.Spock.Lucid
@@ -19,27 +16,20 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 import Web.Spock
 import Lucid.Base
-import Blaze.ByteString.Builder
-
-#if !MIN_VERSION_base(4,8,0)
-import Data.Monoid (mempty)
-#endif
 
 
 -- | Render HTML and send as response body. Content-type will be @text/html@.
 lucid :: MonadIO m => Html a -> ActionCtxT cxt m b
 lucid x = do
   setHeader "Content-Type" "text/html; charset=utf-8"
-  let Identity (render, _) = runHtmlT x
-  lazyBytes (toLazyByteString (render mempty))
+  lazyBytes (renderBS x)
 {-# INLINE lucid #-}
 
 -- | Like 'lucid', but for @HtmlT IO@.
 lucidIO :: MonadIO m => HtmlT IO a -> ActionCtxT cxt m b
 lucidIO x = do
   setHeader "Content-Type" "text/html; charset=utf-8"
-  (render, _) <- liftIO (runHtmlT x)
-  lazyBytes (toLazyByteString (render mempty))
+  lazyBytes =<< liftIO (renderBST x)
 {-# INLINE lucidIO #-}
 
 -- | Like 'lucid', but for arbitrary monads. Might require some additional
@@ -47,6 +37,5 @@ lucidIO x = do
 lucidT :: MonadIO m => HtmlT m a -> ActionCtxT cxt m b
 lucidT x = do
   setHeader "Content-Type" "text/html; charset=utf-8"
-  (render, _) <- lift (runHtmlT x)
-  lazyBytes (toLazyByteString (render mempty))
+  lazyBytes =<< lift (renderBST x)
 {-# INLINE lucidT #-}
